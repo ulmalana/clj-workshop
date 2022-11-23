606449585c2d3f62a3bec13d602b1f73777d87bc
rz <muhamaul@gmail.com>
2022-11-23 12:52:11 +0700

    updated chapter 7

--- ch07-lazy-seq/src/ch07_lazy_seq/core.clj
+++ ch07-lazy-seq/src/ch07_lazy_seq/core.clj
@@ -1,4 +1,8 @@
-(ns ch07-lazy-seq.core)
+(ns ch07-lazy-seq.core
+  (:require [clojure.math.numeric-tower :as math]
+            [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
+            [semantic-csv.core :as sc]))
 
 (defn foo
   "I don't do a whole lot."
@@ -108,3 +112,50 @@
 ;; => ([302 1 302] [278 2 580] [11 3 591])
 (last (take 500000 (average-potatoes '() endless-potatoes)))
 ;; => [353 500000 102208764]
+
+;; lazy trees
+;; exercise 7.03
+(defn match-probability [p1-rating p2-rating]
+  (/ 1
+     (+ 1
+        (math/expt 10 (/ (- p2-rating p1-rating) 400)))))
+
+(def k-factor 32)
+
+(defn recalculate-rating [prev-rating expected-result real-result]
+  (+ prev-rating (* k-factor (- real-result expected-result))))
+
+(defn elo-db [csv k]
+  (with-open [r (io/reader csv)]
+    (->> (csv/read-csv r)
+         sc/mappify
+         (sc/cast-with {:winner_sets_won sc/->int
+                        :loser_sets_won sc/->int
+                        :winner_games_won sc/->int
+                        :loser_games_won sc/->int})
+         (reduce (fn [{:keys [players] :as acc} {:keys [:winner_name :winner_slug :loser_name :loser_slug] :as match}]
+                   (let [winner-rating (get players winner_slug 400)
+                         loser-rating (get players loser_slug 400)
+                         winner-prob (match-probability winner-rating loser-rating)
+                         loser-prob (- 1 winner-prob)]
+                     (-> acc
+                         (assoc-in [:players winner_slug] (recalculate-rating winner-rating winner-prob 1))
+                         (assoc-in [:players loser_slug] (recalculate-rating loser-rating loser-prob 0))
+                         (update :matches (fn [ms]
+                                            (conj ms (assoc match
+                                                            :winner_rating winner-rating
+                                                            :loser_rating loser-rating)))))))
+                 {:players {}
+                  :matches []})
+         :matches
+         reverse)))
+
+(def ratings (elo-db "resources/match_scores_1991-2016_unindexed_csv.csv" 35))
+
+
+(map #(select-keys % [:winner_rating :loser_rating]) (take 5 ratings))
+;; ({:winner_rating 971.7454364144253, :loser_rating 914.5227794278815}
+;;  {:winner_rating 1255.010476602315, :loser_rating 864.1082333776739}
+;;  {:winner_rating 999.9809456512259, :loser_rating 956.9004001481379}
+;;  {:winner_rating 1287.9493288642025, :loser_rating 981.5015812416054}
+;;  {:winner_rating 844.0291961864665, :loser_rating 934.6018166190888})
