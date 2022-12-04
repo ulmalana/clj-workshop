dd436a62d99700fcfa7edb0efb36c7789674bdd8
rz <muhamaul@gmail.com>
2022-12-04 09:37:11 +0700

    updated chapter 10

--- ch10-testing/src/ch10_testing/utils.clj
+++ ch10-testing/src/ch10_testing/utils.clj
@@ -1,4 +1,10 @@
-(ns ch10-testing.utils)
+(ns ch10-testing.utils
+  (:require [clojure.java.io :as io])
+  (:import [java.io PushbackReader]))
+
+(def ^:const currencies
+  {:euro {:countries #{"France" "Spain" :symbol "€"}}
+   :dollar {:countries #{"USA"} :symbol "$"}})
 
 (defn calculate-coffee-price [coffees coffee-type number]
   (-> (get coffees coffee-type)
@@ -7,3 +13,33 @@
 
 (defn display-bought-coffee-message [type number total]
   (println "Buying" number (name type) "coffees for total: $" total))
+
+(defn save-to [location data]
+  (spit location data :append true))
+
+(defn file-exists? [location]
+  (.exists (io/as-file location)))
+
+(defn read-one-order [r]
+  (try
+    (read r)
+    (catch java.lang.RuntimeException e
+      (if (= "EOF while reading" (.getMessage e))
+        ::EOF
+        (throw e)))))
+
+(defn load-orders [file]
+  (if (file-exists? file)
+    (with-open [r (PushbackReader. (io/reader file))]
+      (binding [*read-eval* false]
+        (doall
+         (take-while #(not= ::EOF %)
+                     (repeatedly #(read-one-order r))))))
+    []))
+
+(defn save-coffee-order
+  [orders-file type number price]
+  (save-to orders-file {:type type :number number :price price}))
+
+(defn display-order [order]
+  (str "Bought " (:number order) " cups of " (name (:type order)) " for $ " (:price order)))
--- ch10-testing/test/ch10_testing/utils_test.clj
+++ ch10-testing/test/ch10_testing/utils_test.clj
@@ -1,7 +1,9 @@
 (ns ch10-testing.utils-test
   (:require [clojure.test :refer [are is deftest testing]]
             [ch10-testing.core :refer [price-menu]]
-            [ch10-testing.utils :refer :all]))
+            [ch10-testing.utils :refer :all]
+            [expectations :refer [expect in]]
+            [midje.sweet :refer [=> fact provided unfinished]]))
 
 (deftest calculate-coffee-price-test-with-single-is
   (testing "Single test with is macro"
@@ -21,3 +23,34 @@
       price-menu :latte 1 0.5
       price-menu :latte 2 1.0
       price-menu :latte 3 1.5)))
+
+;; expectations
+(expect 1.5 (calculate-coffee-price price-menu :latte 3))
+
+(expect ClassCastException (calculate-coffee-price price-menu :latte "1"))
+
+;; error test
+;; (expect ClassCastException (calculate-coffee-price price-menu :latte 1))
+
+;; check return type
+(expect Number (calculate-coffee-price price-menu :latte 2))
+
+;; check element in a collection
+(expect {:latte 0.5} (in price-menu))
+
+;; midje
+(fact (calculate-coffee-price price-menu :latte 3) => 1.5)
+
+(unfinished get-currency)
+
+(def test-currency :euro)
+
+(defn get-bought-coffee-message-with-currency
+  [type number total currency]
+  (format "Buying %d %s coffees for total: %s%s" number (name type) (get-currency test-currency) total))
+
+(fact "Message about number of bought coffees should include currency symbol"
+      (get-bought-coffee-message-with-currency :latte 3 1.5 :euro) =>
+      "Buying 3 latte coffees for total: €1.5"
+      (provided
+       (get-currency test-currency) => "€"))
