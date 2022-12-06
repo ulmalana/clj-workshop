72299cffb79d2e98eed7d8ed8a6498226421770d
rz <muhamaul@gmail.com>
2022-12-06 11:24:20 +0700

    added testing suite from chapter 10

--- ch09-support-desk/project.clj
+++ ch09-support-desk/project.clj
@@ -11,17 +11,19 @@
                  [rum "0.12.9"]
                  [funcool/cuerdas "2.2.0"]
                  [jayq "2.5.4"]
-                 [sablono "0.8.6"]]
+                 [sablono "0.8.6"]
+                 [org.clojure/test.check "0.10.0"]]
 
   :source-paths ["src"]
 
   :aliases {"fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
-            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]}
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+            "fig:test" ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "ch09-support-desk.test-runner"]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.17"]
                                   [org.slf4j/slf4j-nop "1.7.30"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
-                   
+
                    :resource-paths ["target"]
                    ;; need to add the compiled assets to the :clean-targets
                    :clean-targets ^{:protect false} ["target"]}})
--- ch09-support-desk/test/ch09_support_desk/core_test.cljs
+++ ch09-support-desk/test/ch09_support_desk/core_test.cljs
@@ -1,10 +1,79 @@
 (ns ch09-support-desk.core-test
     (:require
-     [cljs.test :refer-macros [deftest is testing]]
-     [ch09-support-desk.core :refer [multiply]]))
+     [cljs.test :refer-macros [deftest is testing are use-fixtures]]
+     [clojure.test.check.generators :as gen]
+     [clojure.test.check.properties :refer-macros [for-all]]
+     [clojure.test.check.clojure-test :refer-macros [defspec]]
+     [cuerdas.core :as str]
+     [ch09-support-desk.utils :refer [delete-item-from-list-by-title
+                                      get-priorities-list
+                                      get-sort-message
+                                      get-sorted-priorities-list
+                                      handle-sort-finish]]))
 
-(deftest multiply-test
-  (is (= (* 1 2) (multiply 1 2))))
+(use-fixtures :each
+  {:before (fn [] (do
+                    (def priorities-list [{:title "IE bugs" :priority 2}
+                                          {:title "404 page" :priority 1}
+                                          {:title "Forgotten username" :priority 2}
+                                          {:title "Login token" :priority 1}
+                                          {:title "Mobile version" :priority 3}
+                                          {:title "Load time" :priority 5}])
+                    (def app-state (atom {:sort-counter 0}))))})
 
-(deftest multiply-test-2
-  (is (= (* 75 10) (multiply 10 75))))
+(deftest get-sort-message-test
+  (testing "using is macro"
+    (is (= "little (1)" (get-sort-message 1)))
+    (is (= "medium (4)" (get-sort-message 4)))
+    (is (= "many (8)" (get-sort-message 8))))
+  (testing "using are macro"
+    (are [result number] (= result (get-sort-message number))
+      "little (1)" 1
+      "little (2)" 2
+      "medium (3)" 3
+      "medium (4)" 4
+      "medium (5)" 5
+      "many (6)" 6)))
+
+(defspec get-sort-message-test-check 10
+  (for-all [count gen/nat]
+           (= (str/format "%s (%s)"
+                          (cond
+                            (< count 3) "little"
+                            (< count 6) "medium"
+                            :else "many")
+                          count)
+              (get-sort-message count))))
+
+(deftest get-priorities-list-test
+  (testing "testing filtering priorities based on priority number"
+    (is (= [] (get-priorities-list priorities-list 0)))
+    (is (= [{:title "404 page", :priority 1} {:title "Login token", :priority 1}]
+           (get-priorities-list priorities-list 1)))
+    (is (= [{:title "IE bugs" :priority 2}
+            {:title "404 page" :priority 1}
+            {:title "Forgotten username" :priority 2}
+            {:title "Login token" :priority 1}]
+           (get-priorities-list priorities-list 2)))
+    (is (= [{:title "IE bugs" :priority 2}
+            {:title "404 page" :priority 1}
+            {:title "Forgotten username" :priority 2}
+            {:title "Login token" :priority 1}
+            {:title "Mobile version" :priority 3}]
+           (get-priorities-list priorities-list 3)))))
+
+(deftest delete-item-from-list-by-title-test
+  (testing "Passing empty list"
+    (is (= [] (delete-item-from-list-by-title "Login token" [])))
+    (is (= [] (delete-item-from-list-by-title "Login token" nil))))
+  (testing "Passing valid list"
+    (is (= (delete-item-from-list-by-title "Login token" priorities-list)))))
+
+
+(deftest handle-sort-finish-test
+  (testing "calling fn once"
+    (is (= {:sort-counter 1}
+           ((handle-sort-finish app-state) "event" "object"))))
+  (testing "calling fn twice"
+    (is (= {:sort-counter 2}
+           ((handle-sort-finish app-state) "event" "object")))))
